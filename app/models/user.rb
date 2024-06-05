c953471e3c0bbb037f74d50966ba0962b49c5d6d
Annastacia Mumbua Kioko <annetotoh@gmail.com>
2024-06-05 21:26:51 +0300

    fix longest streak algorithm

--- app/models/user.rb
+++ app/models/user.rb
@@ -93,10 +93,35 @@ class User < ApplicationRecord
   def max_streak
 
     tours = Tour.where.not(base: true).includes(:quiz)
-    quizzes = tours.each { |tour| tour.quiz}
+    quizzes = tours.map { |tour| tour.quiz}
 
     attempts = self.attempts.order(created_at: :desc)
 
-    0
+    result = []
+
+    quizzes.each do |quiz|
+      attempt = attempts.find_by(quiz_id: quiz.id)
+      if attempt
+        result << [quiz.created_at, attempt.created_at]
+      else
+        result << [quiz.created_at, nil]
+      end
+    end
+
+    result.sort_by! { |quiz_date, attempt_date| quiz_date }.reverse!
+
+    current_streak = 0
+    longest_streak = 0
+
+    result.each do |item|
+      if item[1].nil?
+        current_streak = 0
+      else
+        current_streak += 1
+        longest_streak = current_streak if current_streak > longest_streak
+      end
+    end
+
+    longest_streak
   end
 end
