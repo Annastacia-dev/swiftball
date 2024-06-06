096903700cea95a795520baf9a9edb08da6d5b08
Annastacia Mumbua Kioko <annetotoh@gmail.com>
2024-06-06 17:21:53 +0300

    refactor: streak scores to a concern

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,8 +29,9 @@
 #
 class User < ApplicationRecord
   has_paper_trail
-  # Include default devise modules. Others available are:
-  #:lockable, :timeoutable, :trackable and :omniauthable
+
+  include StreakScores
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
@@ -59,69 +60,5 @@ class User < ApplicationRecord
 
   def has_attempted_quiz?(quiz_id)
     attempts.exists?(quiz_id: quiz_id)
-  end
-
-  def current_streak
-    tours = Tour.where.not(base: true).includes(:quiz)
-    quizzes = tours.map { |tour| tour.quiz}
-
-    attempts = self.attempts.order(created_at: :desc)
-
-    result = []
-
-    quizzes.each do |quiz|
-      attempt = attempts.find_by(quiz_id: quiz.id)
-      if attempt
-        result << [quiz.created_at, attempt.created_at]
-      else
-        result << [quiz.created_at, nil]
-      end
-    end
-
-    result.sort_by! { |quiz_date, attempt_date| quiz_date }.reverse!
-
-    streak = 0
-
-    result.each do |pair|
-      break if pair[1].nil?
-      streak += 1
-    end
-
-    streak
-  end
-
-  def max_streak
-
-    tours = Tour.where.not(base: true).includes(:quiz)
-    quizzes = tours.map { |tour| tour.quiz}
-
-    attempts = self.attempts.order(created_at: :desc)
-
-    result = []
-
-    quizzes.each do |quiz|
-      attempt = attempts.find_by(quiz_id: quiz.id)
-      if attempt
-        result << [quiz.created_at, attempt.created_at]
-      else
-        result << [quiz.created_at, nil]
-      end
-    end
-
-    result.sort_by! { |quiz_date, attempt_date| quiz_date }.reverse!
-
-    current_streak = 0
-    longest_streak = 0
-
-    result.each do |item|
-      if item[1].nil?
-        current_streak = 0
-      else
-        current_streak += 1
-        longest_streak = current_streak if current_streak > longest_streak
-      end
-    end
-
-    longest_streak
   end
 end
