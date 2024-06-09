2c6b4457752b6de40d1a38da3d5c3bfef6593002
Annastacia Mumbua Kioko <annetotoh@gmail.com>
2024-06-09 18:07:54 +0300

    fix average score to skip open tours

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -15,7 +15,7 @@ class HomeController < ApplicationController
 
     attempts_for_average = @attempts.includes([:quiz, :responses]).reject { |attempt| attempt.quiz.tour.status == 'open' }
 
-    @average_score = attempts_count.zero? ? 0 : attempts_for_average.map(&:score).sum.to_i / attempts_count
+    @average_score = attempts_count.zero? ? 0 : attempts_for_average.map(&:score).sum.to_i / attempts_for_average.size
     @lifetime_points = attempts_for_average.map(&:score).sum
     @best_score = attempts_for_average.map(&:score).max || 0
 
