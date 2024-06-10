af321c4b758a8b5082bd66c04b6b1b9b100aea0f
Annastacia Mumbua Kioko <annetotoh@gmail.com>
2024-06-10 19:03:10 +0300

    feat: paginate users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,6 @@ class UsersController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @users = User.order(created_at: :desc).where.not(role: 'admin')
+    @users = User.order(created_at: :desc).where.not(role: 'admin').paginate(page: params[:page], per_page: 10)
   end
 end
