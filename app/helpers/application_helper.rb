2b41db10e49d8261ced26458bfc4c204a8eeec0b
Annastacia Mumbua Kioko <annetotoh@gmail.com>
2024-06-10 13:55:23 +0300

    remove absolute class from sidebar + add terms,privacy, disclaimer paths to sidebar, replace icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -118,6 +118,12 @@ module ApplicationHelper
       items << ({ path: 'https://ko-fi.com/annetotoh/goal?g=0', icon_class: 'fa-solid fa-server', menu_text: 'Help Keep Servers Running' })
     end
 
+    items.push({ path: terms_and_conditions_path, icon_class: 'fa-regular fa-newspaper', menu_text: 'Terms & Conditions'})
+
+    items.push({ path: privacy_policy_path, icon_class: 'fa-solid fa-user-lock', menu_text: 'Privacy Policy'})
+
+    items.push({ path: disclaimer_path, icon_class: 'fa-solid fa-triangle-exclamation', menu_text: 'Disclaimer'})
+
     items.push({ path: destroy_user_session_path, icon_class: 'fa-solid fa-right-from-bracket', menu_text: 'Logout', type: 'button', method: :delete })
 
     items
