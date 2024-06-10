cac5fc586e2f68708e8ed67de637b80fda702ada
Annastacia Mumbua Kioko <annetotoh@gmail.com>
2024-06-10 12:54:49 +0300

    fix action error

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[ terms_and_conditions privacy_policy manifest]
+  skip_before_action :authenticate_user!, only: %i[ terms_and_conditions privacy_policy]
 
   def stats
     @chart_type = params[:chart_type] || 'bar_chart'
