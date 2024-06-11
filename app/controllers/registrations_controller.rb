7a37d36c8ff8bdaafd5d47caa8ee089efffca8a6
Annastacia Mumbua Kioko <annetotoh@gmail.com>
2024-06-11 15:46:26 +0300

    dark mode color fixes

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,15 @@ class RegistrationsController < Devise::RegistrationsController
 
   private
 
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
+
   def after_inactive_sign_up_path_for(resource)
     new_user_session_path
+  end
+
+  def account_update_params
+    params.require(:user).permit(:username, :name, :country, :timezone)
   end
 end
