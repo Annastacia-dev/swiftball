08af93e732c2e581148592ec6288f70ecbe04ef6
Annastacia Mumbua Kioko <annetotoh@gmail.com>
2024-08-01 19:33:46 +0300

    show setlist song in analysis

--- app/helpers/setlists_helper.rb
+++ app/helpers/setlists_helper.rb
@@ -1,16 +1,20 @@
 module SetlistsHelper
-def setlist_dropdown_items(setlist)
-  items = [
-    { path: setlist_path(setlist), icon_class: 'fa-solid fa-record-vinyl', menu_text: 'View Setlist' },
-    { path: edit_setlist_path(setlist), icon_class: 'fa-solid fa-pencil', menu_text: 'Edit Setlist' }
-  ]
+  def setlist_dropdown_items(setlist)
+    items = [
+      { path: setlist_path(setlist), icon_class: 'fa-solid fa-record-vinyl', menu_text: 'View Setlist' },
+      { path: edit_setlist_path(setlist), icon_class: 'fa-solid fa-pencil', menu_text: 'Edit Setlist' }
+    ]
 
-  if action_name == 'show'
-    items <<  { path: new_setlist_setlists_song_path(setlist), icon_class: 'fa-solid fa-guitar', menu_text: 'Add a Song', type: 'drawer', id: 'new_setlist_song', view: 'setlists/new_song' }
+    if action_name == 'show'
+      items <<  { path: new_setlist_setlists_song_path(setlist), icon_class: 'fa-solid fa-guitar', menu_text: 'Add a Song', type: 'drawer', id: 'new_setlist_song', view: 'setlists/new_song' }
+    end
+
+    items.push << { path: setlist_path(setlist), icon_class: 'fa-solid fa-delete-left', menu_text: 'Delete Setlist', type: 'button', method: 'delete' }
+
+    items
   end
 
-  items.push << { path: setlist_path(setlist), icon_class: 'fa-solid fa-delete-left', menu_text: 'Delete Setlist', type: 'button', method: 'delete' }
-
-  items
-end
+  def is_in_setlist?(song)
+    Setlist.find_by(status: :previous)&.setlist_songs.find_by(song_id: song.id)&.present?
+  end
 end
