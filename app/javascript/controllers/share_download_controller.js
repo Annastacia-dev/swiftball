ba54189bdc5597dde4debbac9b24285940c31061
Annastacia Mumbua Kioko <annetotoh@gmail.com>
2024-10-26 21:34:02 +0300

    loadImageAsBase64

--- app/javascript/controllers/share_download_controller.js
+++ app/javascript/controllers/share_download_controller.js
@@ -4,7 +4,35 @@ import html2canvas from "html2canvas";
 export default class extends Controller {
   static targets = ["download", "share", "attempt"]
 
-  downloadImage() {
+   loadImageAsBase64 = (url) => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.crossOrigin = "anonymous";
+      img.src = url;
+      img.onload = () => {
+        const canvas = document.createElement("canvas");
+        canvas.width = img.width;
+        canvas.height = img.height;
+        const ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0);
+        resolve(canvas.toDataURL("image/png"));
+      };
+      img.onerror = (err) => reject(err);
+    });
+  };
+
+  async downloadImage() {
+    const images = this.attemptTarget.querySelectorAll("img");
+  
+    for (const img of images) {
+      try {
+        const base64Data = await loadImageAsBase64(img.src);
+        img.src = base64Data;
+      } catch (error) {
+        console.error("Error loading image as base64:", error);
+      }
+    }
+  
     html2canvas(this.attemptTarget, { scale: 2, useCORS: true }).then(canvas => {
       const link = document.createElement("a");
       link.href = canvas.toDataURL("image/png");
@@ -14,6 +42,7 @@ export default class extends Controller {
       console.error("Error capturing canvas:", error);
     });
   }
+  
 
   shareImage() {
     html2canvas(this.attemptTarget).then(canvas => {
